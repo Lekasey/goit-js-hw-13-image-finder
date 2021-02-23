165ff757adef1d4f3fe4d48d90019aa2e4330148
Aleksei Yarmolenko <68028444+Lekasey@users.noreply.github.com>
2021-02-23 02:00:56 +0100

    fix

--- src/js/refs.js
+++ src/js/refs.js
@@ -1,7 +1,6 @@
 const refs = {
     searchBar: document.querySelector('.js-search-bar'),
     searchForm: document.querySelector('.js-search-form'),
-    // btn: document.querySelector('.js-search-btn'),
     resultsContainer: document.querySelector('.js-search-result'),
     body: document.querySelector('body')
 }
